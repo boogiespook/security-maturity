8bf7c8e2b3971874b9540d7a001b22b21b52f905
Chris Jenkins <chrisj@redhat.com>
2024-03-08 08:46:41 +0000

    First drop

---
+++ table-static.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Security Assessment</title>
+<link rel="stylesheet" type="text/css" href="css/overpass.css"/>
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/table2.css">
+
+</head>
+
+<body>
+<div class="bigtable">
+
+<table><thead><tr>
+<th>Rating</th>
+<th>Secure Infrastructure</th>
+<th>Secure Data</th>
+<th>Secure Identity</th>
+<th>Secure Application</th>
+<th>Secure Network</th>
+<th>Secure Recovery</th>
+<th>Secure Operations</th>
+
+</tr></thead>
+<tr>
+<td class="advanced"></td>
+<td class="notcompleted">Identity-Based Perimeter</td>                 
+<td class="notcompleted">Anomaly Detection</td>                        
+<td class="notcompleted">Contextual / Risk Based Access</td>           
+<td class="notcompleted">Interactive Application Security Testing</td> 
+<td class="notcompleted">Zero Trust Network Access</td>                
+<td class="notcompleted">Predictive Recovery</td>                      
+<td class="notcompleted">Purple Teaming</td>                           
+</tr>
+
+<tr>
+<td class="advanced">Advanced</td>
+   
+<td class="notcompleted">Service Mesh Security</td>               
+<td class="notcompleted">Immutable Storage</td>                   
+<td class="notcompleted">AI/ML Anomaly Detection</td>             
+<td class="notcompleted">Runtime Application Self Protection</td> 
+<td class="notcompleted">Microsegmentation</td>                   
+<td class="notcompleted">Advanced Key Management</td>             
+<td class="notcompleted">APT Detection & Response</td>            
+</tr>
+
+<tr>
+<td class="advanced"></td>
+<td class="notcompleted">Container Runtime Security</td>      
+<td class="notcompleted">Automated Posture Management</td>    
+<td class="notcompleted">Identity Federation</td>             
+<td class="notcompleted">Container Scanning</td>              
+<td class="notcompleted">Secure Connections</td>              
+<td class="notcompleted">Storage Scanning & Monitoring</td>   
+<td class="notcompleted">Threat Intelligence Integration</td> 
+
+</tr>
+
+<tr>
+<td class="strategic"></td>
+<td class="notcompleted">Secrets Management</td>                  
+<td class="notcompleted">Loss Prevention</td>                     
+<td class="notcompleted">Privileged Access Managemet</td>         
+<td class="notcompleted">Web Application Firewall</td>            
+<td class="completed">Traffic Analysis</td>                    
+<td class="notcompleted">Lifecycle Management</td>                
+<td class="notcompleted">Orchestration, Automation, Response</td> 
+</tr>
+
+<tr>
+<td class="strategic">Strategic</td>
+<td class="notcompleted">Automated Policy / Enforcement</td>       
+<td class="notcompleted">Tokenization</td>                         
+<td class="notcompleted">Single Sign On</td>                       
+<td class="notcompleted">Dynamic Application Security Testing</td> 
+<td class="completed">Intrusion Detection / Prevention</td>     
+<td class="notcompleted">Automated Failovers</td>                  
+<td class="completed">Endpoint Detection & Response</td>        
+</tr>
+
+<tr>
+<td class="strategic"></td>
+<td class="completed">Logging & Monitoring</td>                    
+<td class="completed">Access Control</td>                          
+<td class="notcompleted">Multi-Factor Identification</td>             
+<td class="notcompleted">Secure Code Practices</td>                   
+<td class="completed">Access Control Lists</td>                    
+<td class="notcompleted">Consistent Versioning</td>                   
+<td class="completed">Security Information & Event Management</td> 
+</tr>
+
+<tr>
+<td class="foundation"></td>
+<td class="completed">Segmentation / Isolation</td>            
+<td class="completed">Encryption</td>                          
+<td class="completed">Role-Based Access Control</td>           
+<td class="completed">Static Application Security Testing</td> 
+<td class="completed">Secure Protocols</td>                    
+<td class="completed">Disaster Recovery Plan</td>              
+<td class="completed">Anti-Virus scan</td>                     
+</tr>
+
+<tr>
+<td class="foundation">Foundation</td>
+<td class="completed">Config Management</td>        
+<td class="completed">Classification</td>           
+<td class="completed">Passwords</td>                
+<td class="completed">Dependency Management</td>    
+<td class="completed">Firewalls & Segmentation</td> 
+<td class="completed">Backup & Redundancy</td>      
+<td class="completed">Incident Response Plan</td>   
+</tr>
+
+</table>
+
+</div>
+
+
+</body>
+</html>
